c1cd6846702fced04667c93e18d518c526c987c5
Daniel Spencer <wym2b@icloud.com>
2025-05-31 15:45:40 +0100

    refactor: improve type safety and searchParams handling in login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,11 +6,15 @@ import { Login } from "./components/Login";
 
 export const dynamic = "force-dynamic";
 
-interface PageProps {
-  searchParams?: { [key: string]: string | string[] | undefined };
+type SearchParams = {
+  [key: string]: string | string[] | undefined;
+};
+
+interface LoginPageProps {
+  searchParams?: SearchParams;
 }
 
-export default async function LoginPage({ searchParams }: PageProps) {
+export default async function LoginPage({ searchParams }: LoginPageProps) {
   const supabase = createServerComponentClient<Database>({ cookies });
 
   const {
@@ -21,12 +25,15 @@ export default async function LoginPage({ searchParams }: PageProps) {
     redirect("/");
   }
 
-  const headersList = headers();
-  const host = (await headersList).get("host");
+  const headersList = await headers();
+  const host = headersList.get("host");
+
+  // Convert searchParams to a plain object if it's a URLSearchParams instance
+  const searchParamsObj = searchParams ? { ...searchParams } : {};
 
   return (
     <div className="flex flex-col flex-1 w-full h-[calc(100vh-73px)]">
-      <Login host={host} searchParams={searchParams} />
+      <Login host={host} searchParams={searchParamsObj} />
     </div>
   );
 }
